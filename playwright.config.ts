104c9107ee9fc3c15e5e8501275401d60acd7510
Piotr S <blackpioter@users.noreply.github.com>
2025-04-25 21:09:23 +0200

    fix: fix playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,16 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [["html", { outputFolder: "./coverage/e2e-report" }], ["list"]],
+  reporter: [
+    ["list"],
+    [
+      "html",
+      {
+        outputFolder: "./coverage/e2e-report",
+        open: "never",
+      },
+    ],
+  ],
   use: {
     baseURL: "http://localhost:3000",
     trace: "on-first-retry",
