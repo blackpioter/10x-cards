481ef477d91ba5e9da1a18ff37fb3099314773ec
Piotr S <blackpioter@users.noreply.github.com>
2025-04-26 15:32:13 +0200

    fix: config playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,6 +9,10 @@ const coverageDir = path.join(process.cwd(), "coverage");
 
 export default defineConfig({
   testDir,
+  timeout: 30000,
+  expect: {
+    timeout: 10000,
+  },
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
@@ -26,6 +30,8 @@ export default defineConfig({
   use: {
     baseURL: "http://localhost:3000",
     trace: "on-first-retry",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
   },
   outputDir: path.join(coverageDir, "screenshots"),
   snapshotDir: path.join(coverageDir, "screenshots"),
@@ -34,7 +40,9 @@ export default defineConfig({
       name: "chromium",
       use: {
         ...devices["Desktop Chrome"],
-        screenshot: "only-on-failure",
+        contextOptions: {
+          ignoreHTTPSErrors: true,
+        },
       },
     },
   ],
