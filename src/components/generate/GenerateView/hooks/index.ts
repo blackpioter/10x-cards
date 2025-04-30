8c0b2ab1d1a0161cfa37dddf0991ede464333731
Piotr S <blackpioter@users.noreply.github.com>
2025-04-30 16:45:17 +0200

    feat: add useGenerate to export

--- src/components/generate/GenerateView/hooks/index.ts
+++ src/components/generate/GenerateView/hooks/index.ts
@@ -1,2 +1,3 @@
 export { useFlashcardStats, type FlashcardStats } from "./useFlashcardStats/useFlashcardStats";
 export { useFlashcardReview } from "./useFlashcardReview/useFlashcardReview";
+export { useGenerate } from "./useGenerate/useGenerate";
